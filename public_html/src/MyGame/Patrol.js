aa807cf570dcd0af3d31ec5aca12c85d272f5667
Matthew Munson <mmunson11@gmail.com>
2020-03-06 16:15:48 -0800

    Implemented visible bounding boxes

--- public_html/src/MyGame/Patrol.js
+++ public_html/src/MyGame/Patrol.js
@@ -17,6 +17,7 @@ function Patrol( spriteSheet, positionX, positionY, velocityX, velocityY, worldW
     
     this.boundingBox = [0, 0, 0, 0];
     
+    this.showBound = false; 
     
     //Create the head - the leader of the patrol (not animated)
     this.head = new SpriteRenderable(this.spriteSheet); 
@@ -45,7 +46,17 @@ function Patrol( spriteSheet, positionX, positionY, velocityX, velocityY, worldW
     this.bottom.setAnimationSpeed(15);
     
     
-
+    this.topBound = [];
+    this.bottomBound = [];
+    this.headBound = [];
+    this.patrolBound = [];
+    
+    this.topBound = this._updateBound(this.top);
+    this.bottomBound = this._updateBound(this.bottom);
+    this.headBound = this._updateBound(this.head);
+    this._updatePatrolBound();
+    
+    
     this.interpolateTopX = new Interpolate(this.positionX, 120, 0.05);
     this.interpolateTopY = new Interpolate(this.positionY, 120, 0.05);
     
@@ -54,6 +65,57 @@ function Patrol( spriteSheet, positionX, positionY, velocityX, velocityY, worldW
 
     this.testCounter1 = 0;
 }
+
+Patrol.prototype._updateBound = function( renderable)
+{
+    var boundArray = [];
+    
+    var xPos = renderable.getXform().getPosition()[0];
+    var yPos = renderable.getXform().getPosition()[1];
+    
+    var height = renderable.getXform().getHeight();
+    var width = renderable.getXform().getWidth();
+    
+    //Top left to top right (top bound)
+    boundArray[0] = new LineRenderable(xPos - width / 2, yPos + height / 2, xPos + width / 2, yPos + height / 2);
+    
+    //Bottom left to bottom right (bottom bound)
+    boundArray[1] = new LineRenderable(xPos - width / 2, yPos - height / 2, xPos + width / 2, yPos - height / 2);
+    
+    //Top left to bottom left (left bound)
+    boundArray[2] = new LineRenderable(xPos - width / 2, yPos + height / 2, xPos - width / 2, yPos - height /2);
+    
+    //Top right to bottom right (right bound)
+    boundArray[3] = new LineRenderable(xPos + width / 2, yPos + height / 2, xPos + width / 2, yPos - height /2);
+    
+    return boundArray;
+};
+
+Patrol.prototype._updatePatrolBound = function()
+{
+    this.patrolBound = [];
+    
+    var width = this.boundingBox[3] - this.boundingBox[2];
+    var height = this.boundingBox[0] - this.boundingBox[1];
+    
+    var xPos = this.boundingBox[2] + width / 2;
+    var yPos = this.boundingBox[1] + height / 2;
+    
+    //Top left to top right (top bound)
+    this.patrolBound[0] = new LineRenderable(xPos - width / 2, yPos + height / 2, xPos + width / 2, yPos + height / 2);
+    
+    //Bottom left to bottom right (bottom bound)
+    this.patrolBound[1] = new LineRenderable(xPos - width / 2, yPos - height / 2, xPos + width / 2, yPos - height / 2);
+    
+    //Top left to bottom left (left bound)
+    this.patrolBound[2] = new LineRenderable(xPos - width / 2, yPos + height / 2, xPos - width / 2, yPos - height /2);
+    
+    //Top right to bottom right (right bound)
+    this.patrolBound[3] = new LineRenderable(xPos + width / 2, yPos + height / 2, xPos + width / 2, yPos - height /2);
+}
+
+
+
 
 // Camden: getters for collisions
 Patrol.prototype.getBoundingBox = function() {return this.boundingBox;};
@@ -68,6 +130,16 @@ Patrol.prototype.draw = function ( camera )
     this.top.draw( camera );
     
     this.bottom.draw( camera );
+    
+    for(var i = 0; i < 4; i++)
+    {
+        this.topBound[i].draw( camera );
+        this.bottomBound[i].draw( camera );
+        this.headBound[i].draw( camera );
+        this.patrolBound[i].draw( camera );
+    }
+    
+    
 };
 
 
@@ -84,6 +156,13 @@ Patrol.prototype.update = function ()
         this.bottomCollision();
     }
     
+    if (gEngine.Input.isKeyClicked(gEngine.Input.keys.B))
+    {
+        this.showBound = !this.showBound;
+    }
+    
+    
+    
     this.top.updateAnimation();
     this.bottom.updateAnimation();
     
@@ -93,6 +172,8 @@ Patrol.prototype.update = function ()
         this.bottom.setSpriteSequence(512 - 164, 0, 204, 160, 5, 0);
         this.testCounter1++;
     }
+    
+    
     
     
     this.positionX += this.velocityX;
@@ -115,6 +196,10 @@ Patrol.prototype.update = function ()
     this.top.getXform().setPosition(this.interpolateTopX.getValue(), this.interpolateTopY.getValue());
     this.bottom.getXform().setPosition(this.interpolateBottomX.getValue(), this.interpolateBottomY.getValue());
     
+    this.topBound = this._updateBound(this.top);
+    this.bottomBound = this._updateBound(this.bottom);
+    this.headBound = this._updateBound(this.head);
+    this._updatePatrolBound();
     
     //Top Bound
     this.boundingBox[0] = this.interpolateTopY.getValue() + 8/2;
@@ -122,11 +207,17 @@ Patrol.prototype.update = function ()
     //Bottom Bound
     this.boundingBox[1] = this.interpolateBottomY.getValue() - 8/2;
     
+    
+    this.boundingBox[0] += (this.boundingBox[0] - this.boundingBox[1]) / 2;
+    
     //Left Bound
     this.boundingBox[2] = this.positionX - 7.5 / 2;
     
     //Right Bound
     this.boundingBox[3] = this.interpolateTopX.getValue() + 10/2;
+    
+    
+    
     
     
     
